ca28883a4b04f4ac39f93a31dd356f4ce670fb01
sindhuravala <sindhu01.ravala@gmail.com>
2022-07-08 00:15:32 +0300

    index file

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended: true})); 
 app.use(bodyParser.json()); 
-app.use(cors({ credentials:true, origin:'https://sinduaapp.herokuapp.com/' }));
+app.use(cors({ credentials:true, origin:'https://sinduaapp.herokuapp.com' }));
 app.use(express.static(__dirname + "/public"));
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public', 'index.html')));
 app.use(function(req, res, next) {
